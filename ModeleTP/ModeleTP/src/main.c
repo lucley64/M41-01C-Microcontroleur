9ba3834cf5e7ecf23fcad5b374aace0988544ca6
LEYDERT Luc <lleydert@iutbayonne.univ-pau.fr>
2022-03-04 10:48:09 +0100

    Tourner la base : check

--- ModeleTP/ModeleTP/src/main.c
+++ ModeleTP/ModeleTP/src/main.c
@@ -1,11 +1,30 @@
 #include "libTPIUT.h"
 #include "robot.h"
 
-int main (void)
-{
+#define BP1 25
+#define MOT_BASE 5
+
+void init(){
 	initCarte();
 	configureConsole();
-	while(true) {
+	initRobot();
+	allumerPeriph(PortB);
+	programmerLigne(PortB, BP1, ENTREE);
+	activerFiltrageLigne(PortB, BP1, 20);
+}
 
+int main (void)
+{
+	init();
+	int posision[] = {10, 0, -10, 0};
+	int i = 0;
+	while(true) {
+		if(lireLigne(PortB, BP1)){
+			positionnerMoteur(BASE, posision[i]);
+			printf("Bouton appuié");
+			i = (i+1)%4;
+			while(lireLigne(PortB, BP1));
+		}
 	}
 }
+
