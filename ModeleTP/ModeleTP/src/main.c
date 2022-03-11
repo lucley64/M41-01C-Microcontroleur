ca7f885a63849e604fac9518e3c05fbd28d84693
LEYDERT Luc <lleydert@iutbayonne.univ-pau.fr>
2022-03-11 09:43:05 +0100

    Exercice 2 TP2

--- ModeleTP/ModeleTP/src/main.c
+++ ModeleTP/ModeleTP/src/main.c
@@ -7,6 +7,9 @@
 #define DEL_MILLIEU 21
 #define DEL_DROITE 27
 #define TIMER Timer1
+#define TIMER_RESET Timer0
+
+void resetMot();
 
 void init(){
 	initCarte();
@@ -26,6 +29,10 @@ void init(){
 	
 	allumerPeriph(TIMER);
 	timerModeDelai(TIMER, HDIV2, 1680000UL, REPETITIF, INC);
+	
+	allumerPeriph(TIMER_RESET);
+	timerModeDelai(TIMER_RESET,HDIV2, 42000000UL, NON_REPETITIF, INC);
+	autoriserITTimer(TIMER_RESET, LIMITE, 0, resetMot);
 }
 
 float positionnerMoteurLent(int moteur, float angle, float prevAngle){
@@ -46,40 +53,57 @@ float positionnerMoteurLent(int moteur, float angle, float prevAngle){
 	return angle;
 }
 
-int main (void)
-{
+int indicePos[] = {0, 0, 0, 0, 0};
+float prevAngle[] = {0, 0, 0, 0, 0};
+int isInter = 0;	
+
+void resetMot(){
+	for (int i = 0; i < 5; i++){
+		prevAngle[i] = positionnerMoteurLent(i, 0, prevAngle[i]);
+		indicePos[i] = 0;
+	}
+	isInter = 1;
+}
+
+int main (void) {
 	init();
 	int posision[] = {30, 0, -30, 0};
 	int del[] = {DEL_GAUCHE, DEL_MILLIEU, DEL_DROITE};
 	int portsDel[] = {PortC, PortA, PortB};
-	int indicePos[] = {0, 0, 0, 0, 0};
 	int indiceMot = 0;
-	float prevAngle[] = {0, 0, 0, 0, 0};
 	/// TP2 : 
 	
 	//while (1){
 		//if(lireLigne(PortB, BP1)){
-			//prevAngle = positionnerMoteurLent(BASE, posision[indicePos], prevAngle);
-			//indicePos = (indicePos+1)%4;
+			//lancerTimer(TIMER_RESET);
 			//while(lireLigne(PortB, BP1));
+			//arreterTimer(TIMER_RESET);
+			//if (!isInter){
+				//prevAngle[0] = positionnerMoteurLent(BASE, posision[indicePos[0]], prevAngle[0]);
+				//indicePos[0] = (indicePos[0]+1)%4;
+				//while(lireLigne(PortB, BP1));
+			//}
+			//isInter = 0;
 		//}
 	//}
 	
-	/// TP1 : 
+	/// TP1 et 2 : 
 	while(true) {
 		if(lireLigne(PortB, BP1)){
-			if(indiceMot == 5){
-				for (int i = 0; i < 5; i++){
-					prevAngle[i] = positionnerMoteurLent(i, 0, prevAngle[i]);
-					indicePos[i] = 0;
+			lancerTimer(TIMER_RESET);
+			while(lireLigne(PortB, BP1));
+			arreterTimer(TIMER_RESET);
+			if (!isInter) {
+				if(indiceMot == 5){
+					resetMot();
+				}
+				else{
+					printf("BP1 appuié\n");
+					prevAngle[indiceMot] = positionnerMoteurLent(indiceMot, posision[indicePos[indiceMot]], prevAngle[indiceMot]);
+					indicePos[indiceMot] = (indicePos[indiceMot]+1)%4;
 				}
 			}
-			else{
-				printf("BP1 appuié\n");
-				prevAngle[indiceMot] = positionnerMoteurLent(indiceMot, posision[indicePos[indiceMot]], prevAngle[indiceMot]);
-				indicePos[indiceMot] = (indicePos[indiceMot]+1)%4;
-				while(lireLigne(PortB, BP1));
-			}
+			isInter = 0;
 		}
 		if(lireLigne(PortC, BP2)){
 			printf("BP2 appuié\n");
@@ -94,9 +118,9 @@ int main (void)
 					}
 					ecrireLigne(portsDel[i], del[i], bin);
 					indiceMotToBin /= 2;
-					}
+				}
 				printf("\n");
-			} 
+			}
 			else{
 				ecrireLigne(PortC, DEL_GAUCHE, 0);
 				ecrireLigne(PortA, DEL_MILLIEU, 0);
@@ -106,4 +130,3 @@ int main (void)
 		}
 	}
 }
-
